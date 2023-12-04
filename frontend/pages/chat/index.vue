e10858f4783d35407d892304483c4f3681b7f718
sunset513 <amy39899009@gmail.com>
2023-12-04 12:35:53 +0800

    add : main page and chat page

---
+++ frontend/pages/chat/index.vue
@@ -0,0 +1,73 @@
+<script setup>
+// import  ChatFreinds  from '#build/components';
+</script>
+<template>
+	<!-- temp navbar -->
+	<div class=" bg-#E8DEF8 w-screen h-[8vh]">
+		<div class=" bg-#E8DEF8 w-screen h-[8vh]">
+			<div>
+				<h1> this is temp navbar</h1>
+			</div>
+
+		</div>
+		<div class="flex h-full">
+			<!-- 左邊對話列表 -->
+			<div class="w-1/5 h-[100vh] bg-indigo-300">
+				<div class="h-[12vh] bg-indigo-100 flex">
+					<div>
+						<img class=" mx-auto ml-[1rem] w-[4vw] h-[4vw] rounded-full" src="https://via.placeholder.com/95x96" />
+					</div >
+					<div class=" mx-auto ml-[1rem] text-black text-xl font-normal font-['Roboto'] leading-10"> create group</div>
+				</div>
+
+				<!-- 好友名單 -->
+				<div>
+					<div class="flex relative my-2">
+						<div>
+							<img class="w-[4vw] h-[4vw] ml-[0.5rem] rounded-full" src="https://via.placeholder.com/95x96" />
+						</div>
+						<div>
+							<div class="w-[4vw] h-[4vw] mx-auto ml-[1rem] text-black text-xl font-normal font-['Roboto'] leading-10">
+								benbenboo</div>
+						</div>
+					</div>
+					<div class="flex relative my-2">
+						<div>
+							<img class="w-[4vw] h-[4vw] ml-[0.5rem] rounded-full" src="https://via.placeholder.com/95x96" />
+						</div>
+						<div>
+							<div class="w-[4vw] h-[4vw] mx-auto ml-[1rem] text-black text-xl font-normal font-['Roboto'] leading-10">
+								benbenboo</div>
+						</div>
+					</div>
+				</div>
+				<div class="flex relative my-2">
+					<div>
+						<img class="w-[4vw] h-[4vw] ml-[0.5rem] rounded-full" src="https://via.placeholder.com/95x96" />
+					</div>
+					<div>
+						<div class="w-[4vw] h-[4vw] mx-auto ml-[1rem] text-black text-xl font-normal font-['Roboto'] leading-10">
+							benbenboo</div>
+					</div>
+				</div>
+				<div class="flex relative my-2">
+					<div>
+						<img class="w-[4vw] h-[4vw] ml-[0.5rem] rounded-full" src="https://via.placeholder.com/95x96" />
+					</div>
+					<div>
+						<div class="w-[4vw] h-[4vw] mx-auto ml-[1rem] text-black text-xl font-normal font-['Roboto'] leading-10">
+							benbenboo</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- 右邊聊天室 -->
+			<div class="w-4/5 h-[100vh] bg-indigo-200">
+				<div class="w-2/3  h-[6vh] top-[90vh] left-[70vh] mx-auto bg-zinc-300 bg-opacity-80 rounded-3xl z-10   fixed">
+					<h1 class="items-end">hello</h1>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</template>
--- frontend/pages/main/index.vue
+++ frontend/pages/main/index.vue
@@ -1,6 +1,11 @@
-<script>
-import { ref } from 'vue'
-import TempViewPost from '~/components/TempViewPost.vue';
+<script setup>
+// import PostPreview from './components/PostPreview.vue';
+// components: {
+//     PostPreView
+//   }
+
+// other component options...
+
 
 </script>
 <template>
@@ -17,7 +22,7 @@ import TempViewPost from '~/components/TempViewPost.vue';
 
       <!-- 左半邊 -->
       <div class="flex">
-        <div class=" w-9/12 bg-blue-500 h-full">
+        <div class=" w-9/12 bg-indigo-100 h-full">
           <div class=" bg-white h-[10vh]">
             <h1> </h1>
           </div>
@@ -27,93 +32,92 @@ import TempViewPost from '~/components/TempViewPost.vue';
             </div>
           </div>
 
-
-          <!-- 預覽文章 -->
-          <div class="w-full h-[28vh]  box-border relative flex justify-start items-start inline-flex">
+          <PostPreview></PostPreview>
+          <!-- 預覽文章1 -->
+          <div class="w-full h-[24vh]  box-border relative flex items-start inline-flex">
             <div class="w-[8vw] bg-white"></div>
             <div class=" w-3/4 bg-green-100">
               <div class="w-full h-[7vh] bg-green-250 text-black text-3xl font-semibold font-['Roboto'] leading-10">
                 輕鬆學會前端框架 vue實做 : 裕翔老師</div>
               <div class="w-full h-[10vh] left-[1rem]">
                 <div class=" bg-green-300 text-zinc-600 text-l font-normal font-['Roboto'] ">
-                  這篇文章裡，我們會從 Vue.js 基本概念開始講起，非常適合剛開始接觸 Vue 的朋友們，接著我們用 Vue component 實作一個動態的飯店編輯頁面
+                  這篇文章裡，我們會從 Vue.js 基本概念開始講起，非常適合剛開始接觸 Vue 的朋友們
+                  ，接著我們用 Vue component 實作一個動態的飯店編輯頁面
                 </div>
               </div>
 
-              <div class="w-full h-[11vh] bg-green-350">
-                <div class="w-12/96 mx-[1rem]  bg-red-100 rounded-full"> </div>
-                <div class="w-36/96 bg-green-200 text-black text-2xl font-normal font-['Roboto'] leading-loose">benbenboo
+              <div class="w-full h-[7vh] bg-green-350 mx-2 flex">
+                <img class=" w-12/96 h-12/96 rounded-full  mx-2" src="https://via.placeholder.com/95x96" />
+                <div class="w-36/96 bg-green-200 text-black text-xl font-normal font-['Roboto'] leading-loose">
+                  benbenboo
                 </div>
               </div>
             </div>
 
-            <div class=" w-1/4 h-[7vh] bg-green-500 ">
-              <img class=" my-2 " src="https://via.placeholder.com/240x148" />
+            <div class=" w-1/4 h-[24vh] bg-green-500 ">
+              <img class=" my-1 " src="https://via.placeholder.com/240x148" />
             </div>
             <div class="w-[8vw] bg-white"></div>
           </div>
 
-                    <div class="w-full h-[28vh]  box-border relative flex justify-start items-start inline-flex">
-              <div class="w-[8vw] bg-white"></div>
-              <div class=" w-3/4 bg-green-100">
-                <div class="w-full h-[7vh] bg-green-250 text-black text-3xl font-semibold font-['Roboto'] leading-10">
-                  輕鬆學會前端框架 vue實做 : 裕翔老師</div>
-                <div class="w-full h-[10vh] left-[1rem]">
-                  <div class=" bg-green-300 text-zinc-600 text-l font-normal font-['Roboto'] ">
-                    這篇文章裡，我們會從 Vue.js 基本概念開始講起，非常適合剛開始接觸 Vue 的朋友們，接著我們用 Vue component 實作一個動態的飯店編輯頁面
-                  </div>
-                </div>
-
-                <div class="w-full h-[11vh] bg-green-350">
-                  <div class="w-12/96 mx-[1rem]  bg-red-100 rounded-full"> </div>
-                  <div class="w-36/96 bg-green-200 text-black text-2xl font-normal font-['Roboto'] leading-loose">benbenboo
-                  </div>
+          <!-- 預覽文章2 -->
+          <div class="w-full h-[24vh]  box-border relative flex  items-start inline-flex">
+            <div class="w-[8vw] bg-white"></div>
+            <div class=" w-3/4 bg-green-100">
+              <div class="w-full h-[7vh] bg-green-250 text-black text-3xl font-semibold font-['Roboto'] leading-10">
+                輕鬆學會前端框架 vue實做 : 裕翔老師</div>
+              <div class="w-full h-[10vh] left-[1rem]">
+                <div class=" bg-green-300 text-zinc-600 text-l font-normal font-['Roboto'] ">
+                  這篇文章裡，我們會從 Vue.js 基本概念開始講起，非常適合剛開始接觸 Vue 的朋友們
+                  ，接著我們用 Vue component 實作一個動態的飯店編輯頁面
                 </div>
               </div>
 
-              <div class=" w-1/4 h-[7vh] bg-green-500 ">
-                <img class=" my-2 " src="https://via.placeholder.com/240x148" />
+              <div class="w-full h-[7vh] bg-green-350 mx-2 flex">
+                <img class=" w-12/96 h-12/96 rounded-full  mx-2" src="https://via.placeholder.com/95x96" />
+                <div class="w-36/96 bg-green-200 text-black text-xl font-normal font-['Roboto'] leading-loose">
+                  benbenboo
+                </div>
               </div>
-              <div class="w-[8vw] bg-white"></div>
             </div>
-            
-                      <div class="w-full h-[28vh]  box-border relative flex justify-start items-start inline-flex">
-              <div class="w-[8vw] bg-white"></div>
-              <div class=" w-3/4 bg-green-100">
-                <div class="w-full h-[7vh] bg-green-250 text-black text-3xl font-semibold font-['Roboto'] leading-10">
-                  輕鬆學會前端框架 vue實做 : 裕翔老師</div>
-                <div class="w-full h-[10vh] left-[1rem]">
-                  <div class=" bg-green-300 text-zinc-600 text-l font-normal font-['Roboto'] ">
-                    這篇文章裡，我們會從 Vue.js 基本概念開始講起，非常適合剛開始接觸 Vue 的朋友們，接著我們用 Vue component 實作一個動態的飯店編輯頁面
-                  </div>
-                </div>
 
-                <div class="w-full h-[11vh] bg-green-350">
-                  <div class="w-12/96 mx-[1rem]  bg-red-100 rounded-full"> </div>
-                  <div class="w-36/96 bg-green-200 text-black text-2xl font-normal font-['Roboto'] leading-loose">benbenboo
-                  </div>
-                </div>
-              </div>
-
-              <div class=" w-1/4 h-[7vh] bg-green-500 ">
-                <img class=" my-2 " src="https://via.placeholder.com/240x148" />
-              </div>
-              <div class="w-[8vw] bg-white"></div>
+            <div class=" w-1/4 h-[24vh] bg-green-500 ">
+              <img class=" my-1 " src="https://via.placeholder.com/240x148" />
             </div>
+            <div class="w-[8vw] bg-white"></div>
+          </div>
+
 
         </div>
 
         <!-- 右半邊 -->
-        <div class=" w-3/12 bg-blue-500 h-full ">
+        <div class=" w-3/12 bg-indigo-200 h-full ">
           <div class=" h-[100vh]">
             <!-- 熱門標籤 -->
-            <div class=" h-[50vh] bg-blue-400 border-b-4 border-indigo-500">
+            <div class=" h-[50vh] bg-indigo-300 border-b-4 border-indigo-500">
+              <div class="w-[20vw] mx-auto">
+                <div class=" text-black text-xl font-normal font-['Roboto']">Trending Topics/Tags</div>
+              </div>
 
+              <div class="flex">
+                <div class="mx-auto rounded-sm border border-stone-400 text-center 
+                text-neutral-900 text-l font-normal font-['Roboto'] leading-7">世界大賽
+                </div>
+                <div class="mx-auto rounded-sm border border-stone-400 text-center 
+                text-neutral-900 text-l font-normal font-['Roboto'] leading-7">世界大賽
+                </div>
+                <div class="mx-auto rounded-sm border border-stone-400 text-center 
+                text-neutral-900 text-l font-normal font-['Roboto'] leading-7">世界大賽
+                </div>
+              </div>
             </div>
 
             <!-- 活動 -->
-            <div class=" h-[50vh] justify-center">
-              <div class="w-4/12 h-[10vh] relative ">
+            <div class=" h-[50vh] ">
+              <div class="w-[16vw] mx-auto">
+                <div class=" text-black text-xl font-normal font-['Roboto']">Recent Activity</div>
+              </div>
+              <div class="w-3/12 h-[10vh] mx-[2rem] relative ">
                 <div class="w-[20vw] h-[10vh]  bg-purple-200 rounded-3xl  top-[1rem] absolute 
                       text-center text-neutral-900 text-xl font-normal font-['Roboto']">DevFest Tainan 2023
                   <br />
