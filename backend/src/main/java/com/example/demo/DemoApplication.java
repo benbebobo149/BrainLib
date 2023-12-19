7a5d22f4deca4b3ec8dc2bf78e228f49fc14c9db
OG_Matcha <matchaskill@gmail.com>
2023-12-19 23:19:00 +0800

    hotfix: I'm so good

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+// import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableWebSecurity
 @RestController
 public class DemoApplication{
     public static void main(String[] args) {
@@ -21,17 +22,17 @@ public class DemoApplication{
     }
 
     
-    @Configuration
-    @EnableWebSecurity
-    public class WebSecurityConfig extends WebSecurityConfigurer<HttpSecurity> {
-        @Override
-        public void configure(HttpSecurity http) throws Exception {
-            http
-                .authorizeRequests()
-                    .anyRequest().authenticated()
-                    .and()
-                .exceptionHandling()
-                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
-        }
-    }
+    // @Configuration
+    // @EnableWebSecurity
+    // public class WebSecurityConfig extends WebSecurityConfigurer<HttpSecurity> {
+    //     @Override
+    //     public void configure(HttpSecurity http) throws Exception {
+    //         http
+    //             .authorizeRequests()
+    //                 .anyRequest().authenticated()
+    //                 .and()
+    //             .exceptionHandling()
+    //                 .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+    //     }
+    // }
 }
