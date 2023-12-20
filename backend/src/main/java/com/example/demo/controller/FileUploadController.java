2fe665a53d3c5265ec15b51624be0bec3fca3285
benbebobo149 <benbebobo149@gmail.com>
2023-12-20 23:23:48 +0800

    for debug

--- backend/src/main/java/com/example/demo/controller/FileUploadController.java
+++ backend/src/main/java/com/example/demo/controller/FileUploadController.java
@@ -10,32 +10,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.io.IOException;
 
 @RestController
 public class FileUploadController {
-
+    
+    @Value("${my.custom.imgurToken}")
+    private String imgurToken;
     @PostMapping("/uploadFile")
     public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+        
         HttpClient httpclient = HttpClients.createDefault();
         HttpPost httppost = new HttpPost("https://api.imgur.com/3/image");
-        httppost.setHeader("Authorization", "Client-ID {your-client-id}");
+        httppost.setHeader("Authorization", "Bearer " + imgurToken);
 
-        // HttpEntity reqEntity = MultipartEntityBuilder.create()
-        //         .addBinaryBody("image", file.getInputStream(), ContentType.create(file.getContentType()), file.getOriginalFilename())
-        //         .build();
+        HttpEntity reqEntity = MultipartEntityBuilder.create()
+                .addBinaryBody("image", file.getInputStream(), ContentType.create(file.getContentType()), file.getOriginalFilename())
+                .build();
 
-        // httppost.setEntity(reqEntity);
+        httppost.setEntity(reqEntity);
 
-        // HttpResponse response = httpclient.execute(httppost);
-        // HttpEntity resEntity = response.getEntity();
+        HttpResponse response = httpclient.execute(httppost);
+        HttpEntity resEntity = response.getEntity();
 
-        // if (resEntity != null) {
-        //     String responseString = EntityUtils.toString(resEntity);
-        //     // Parse the responseString to get the image link
-        //     // Return the image link
-        // }
+        if (resEntity != null) {
+            String responseString = EntityUtils.toString(resEntity);
+            // Parse the responseString to get the image link
+            // Return the image link
+            return responseString;
+        }
 
         return null;
     }
