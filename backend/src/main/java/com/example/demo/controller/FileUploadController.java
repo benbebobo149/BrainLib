d8bb6ea7af63bcb215e37818c72372349322680e
benbebobo149 <benbebobo149@gmail.com>
2023-12-24 18:27:57 +0800

    feat: cors finish

--- backend/src/main/java/com/example/demo/controller/FileUploadController.java
+++ backend/src/main/java/com/example/demo/controller/FileUploadController.java
@@ -1,5 +1,4 @@
 package com.example.demo.controller;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 import org.json.JSONObject;
@@ -22,16 +20,15 @@ import java.util.HashMap;
 import java.util.Map;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-
 @RestController
 public class FileUploadController  {
     
     @Value("${my.custom.imgurToken}")
     private String imgurToken;
 
-    @CrossOrigin(origins = "http://localhost:3000/")
-    @GetMapping("/uploadFile")
-    public ResponseEntity<Map<String, String>> handleFileUpload(@RequestParam("file") MultipartFile file) {
+    @CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/uploadFile")
+    public ResponseEntity<Map<String, Object>> handleFileUpload(@RequestParam("image") MultipartFile file) {
     HttpClient httpclient = HttpClients.createDefault();
     HttpPost httppost = new HttpPost("https://api.imgur.com/3/image");
     httppost.setHeader("Authorization", "Bearer " + imgurToken);
@@ -50,13 +47,16 @@ public class FileUploadController  {
           String responseString = EntityUtils.toString(resEntity);
           JSONObject jsonObject = new JSONObject(responseString);
           String imageUrl =  jsonObject.getJSONObject("data").getString("link");
-          Map<String, String> responseBody = new HashMap<>();
-          responseBody.put("url", imageUrl);
-          return new ResponseEntity<Map<String, String>>(responseBody, HttpStatus.OK);
+          Map<String, Object> fileMap = new HashMap<>();
+          fileMap.put("url", imageUrl);
+          Map<String, Object> responseBody = new HashMap<>();
+          responseBody.put("success", 1);
+          responseBody.put("file", fileMap);
+          return new ResponseEntity<Map<String, Object>>(responseBody, HttpStatus.OK);
       }
     } catch (IOException e) {
-      return new ResponseEntity<Map<String, String>>(HttpStatus.INTERNAL_SERVER_ERROR);
+      return new ResponseEntity<Map<String, Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
-      return new ResponseEntity<Map<String, String>>(HttpStatus.INTERNAL_SERVER_ERROR);
+      return new ResponseEntity<Map<String, Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
