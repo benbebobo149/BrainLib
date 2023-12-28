1b012ecba163b100a39c7eb1cafa7bc36269b6c1
sunset513 <amy39899009@gmail.com>
2023-12-29 04:30:12 +0800

    add:  admini page suspost

--- frontend/components/Admini/AdiminiReportPost.vue
+++ frontend/components/Admini/AdiminiReportPost.vue
@@ -1,44 +1,74 @@
 
-<script setup>
-import PostFakeDatas from '@/public/PostFakeData/PostFakeData.json'
 
-const Posts = ref(PostFakeDatas);
+<script setup>
+
+//get data from database
+import axios from 'axios';
+const config = useRuntimeConfig();
+
+const Posts = ref([]);
+
+//get data from database
+const getAllPost = () => {
+	const token = useCookie('token');
+	axios.get(`${config.public.apiURL}/post/suspend`, { // config.public.apiURL + "/tag"
+	}, {
+		headers: {
+			'Authorization': 'Bearer ' + token.value,
+			'Content-Type': 'application/json',
+			'accept': 'application/json'
+		}
+	})
+		.then((res) => {
+			// if code is 200, then hide the modal
+			console.log(res);
+			if (res.status == 200) {
+				console.log("success to get all post");
+				Posts.value = res.data;
+			}
+		})
+		.catch((err) => {
+			// if code is 401, then show error message 
+			console.log(err);
+			if (err.response.status == 404) {
+				console.log("fail to get all post");
+			} else if (err.response.status == 500) {
+				console.log("fail to get all post");
+			}
+		})
+}
+
+getAllPost();
+
 // define emit
 const emit = defineEmits(['close'])
 
+
 // define props
-const props = defineProps({
-    post: {
-        type: Object,
-        default: () => ({
-            title: 'Not Found', author: 'Author Not Found',
-            content: 'Content Not Found', topic: 'Topic ', likes: 0, comments: 0
-        })
-    }
-});
+// const props = defineProps({
+// 	post: {
+// 		type: Object,
+// 		default: () => ({
+// 			title: 'Not Found', author: 'Author Not Found',
+// 			content: 'Content Not Found', topic: 'Topic ', likes: 0, comments: 0
+// 		})
+// 	}
+// });
 // change props to ref 
-const { post } = toRefs(props);
+// const { post } = toRefs(props);
 
 // define methods
 const clickButton = () => {
-    console.log('click button')
-    emit('close')
-}
-const passPost = () => {
-    emit('passPost', post.value);
-    console.log(post.value);
-}
-const rejectPost = () => {
-    emit('rejectPost', post.value);
-    console.log(post.value);
+	console.log('click button')
+	emit('close')
 }
 
 </script>
 
 <template>
-    <div v-for="post in Posts" :key="post.post_id" class="w-[70vw] flex mx-1 z-0">
+    <div v-for="post in Posts" :key="post.post_id" class="flex mx-1 z-0">
         <!-- 預覽文章1 -->
-        <div class="w-full h-[22vh]  box-border my-[0.7rem] relative flex items-start ">
+        <div class="w-[72vw] h-[24vh]  box-border my-[0.7rem] relative flex items-start ">
             <div class="w-[8vw] bg-bgcolor"></div>
             <div class=" w-3/4 bg-bgcolor">
                 <div class="w-full h-[7vh] bg-green-250 text-black text-[2vw] font-semibold font-Roboto leading-10">
@@ -60,7 +90,7 @@ const rejectPost = () => {
                     <div class="w-[21vw] h-[5vw] flex justify-center">
                         <div v-for="tag in post.tags" :key="tag"
                             class="w-[7vw] h-[1.5vw] ml-[1vw] bg-purple-200 rounded  text-center text-neutral-900 text-[1vw] font-normal font-'Roboto'">
-                            <p>{{ tag.tag_name }}</p>
+                            <p>{{ tag.tagName }}</p>
                         </div>
                     </div>
                     <div class="w-[12vw] flex"><img class=" w-[4vh] h-[4vh] ml-[1vw] " src="@/PostPreview/Thumb.png" />
@@ -73,19 +103,24 @@ const rejectPost = () => {
                             {{ post.comments }}
                         </h2>
                     </div>
-                </div>
-            </div>
-            <div class=" w-1/4 h-[18vh]">
-                <img class="w-full h-[10vw] my-1 " src="https://via.placeholder.com/240x148" />
-            </div>
-        </div>
 
-        <div class="h-[full]  bg-bgcolor ">
-            <div class="content-center flex ">
-                <img class="h-[3vw] w-[3vw] mx-[2vh] my-[8vh] cursor-pointer items-center"
-                    src="@/PageAdmini/XCircleFill.png" alt="no image found" @click="rejectPost">
-                <img class="h-[3vw] w-[3vw] mx-[2vh] my-[8vh] cursor-pointer items-center"
-                    src="@/PageAdmini/CheckCircleFill.png" alt="no image found" @click="passPost">
+                </div>
+
+
+            </div>
+
+            <div class=" w-1/4 h-[22vh] ">
+                <img class="w-full h-[10vw] mb-[3vh] rounded-md" :src=post.image />
+            </div>
+            <div class="w-[8vw] bg-bgcolor">
+                <div class="h-[full]  bg-bgcolor ">
+                    <div class="content-center flex ">
+                        <img class="h-[3vw] w-[3vw] mx-[2vh] my-[8vh] cursor-pointer items-center"
+                            src="@/PageAdmini/XCircleFill.png" alt="no image found" @click="rejectPost">
+                        <img class="h-[3vw] w-[3vw] mx-[2vh] my-[8vh] cursor-pointer items-center"
+                            src="@/PageAdmini/CheckCircleFill.png" alt="no image found" @click="passPost">
+                    </div>
+                </div>
             </div>
         </div>
 
